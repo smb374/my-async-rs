ff17ca8b42e418208f9b8c6447b5cffe93a3b2fd
Po-Yeh Chen <smb374.cs08@nctu.edu.tw>
2022-11-23 10:09:51 +0800

    Update document build script and host book pages.

--- document-hook.sh
+++ document-hook.sh
@@ -1,5 +1,10 @@
 #!/bin/sh
 cargo doc --no-deps
-rm -rf ./docs/api_references
+rm -rf ./docs/*
 echo "<meta http-equiv=\"refresh\" content=\"0; url=my_async\">" > target/doc/index.html
 cp -r target/doc ./docs/api_references
+
+cd book
+mdbook build
+cp -r book ../docs/design
+cd -
